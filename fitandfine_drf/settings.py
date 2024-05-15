be6a6fa4d35b407fa10e4afb0e5c55fc0ff2b9d2
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-15 10:49:49 +0000

    Date and time formatting

--- fitandfine_drf/settings.py
+++ fitandfine_drf/settings.py
@@ -32,7 +32,8 @@ REST_FRAMEWORK = {
         else 'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
     )],
     'DEFAULT_PAGINATION_CLASS':  'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DATETIME_FORMAT': '%d %b %Y'
     }
 
 if 'DEV' not in os.environ:
