7c57009497a4c2b2cecf8b642552395e322564a5
swathi keshavamurthy <swathik.2197@gmail.com>
2024-05-16 08:28:07 +0000

    Register the models in admin.py of collaborate

--- collaborate/admin.py
+++ collaborate/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import About, CollaborationRequest
 
-# Register your models here.
+admin.site.register(About)
+admin.site.register(CollaborationRequest)
